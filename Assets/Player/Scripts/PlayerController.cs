f9dd589fe39c3e674c1f22f2bf33575f8e017338
halenf <s210172@students.aie.edu.au>
2024-07-24 15:53:44 +0930

    Added things for player
    Imported TMP Essentials data (forgot).
    Created Player Model prefab.
    Created Player prefab.
    Finished Player Controller script.
    - Movement
    - Jumping
    - Looking
    Added some debug tools to Player Interaction script for now.
    Worked in PlayerControllerTest scene.

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -5,8 +5,7 @@
 /// 
 ///</summary>
 
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,17 +15,127 @@ namespace Millivolt
     {
         public class PlayerController : MonoBehaviour
         {
-            // components
-            private CharacterController m_characterController;
+            void Start()
+            {
+                m_collider = GetComponent<CapsuleCollider>();
+                cursorIsLocked = true;
+            }
+
+            private void FixedUpdate()
+            {
+                MovePlayer();
+            }
+
+            [Header("Physics")]
+            [SerializeField] private float m_mass;
+            [SerializeField] private Vector3 m_gravity;
+
+            public void SetMass(float value)
+            {
+                m_mass = value;
+            }
+
+            public void SetGravity(Vector3 value)
+            {
+                m_gravity = value;
+            }
 
             [Header("Movement")]
             [SerializeField] private float m_moveSpeed;
+            private CapsuleCollider m_collider;
             private Vector2 m_moveDirection;
 
+            public void Move(InputAction.CallbackContext context)
+            {
+                m_moveDirection = context.ReadValue<Vector2>();
+            }
+
+            /// <summary>
+            /// Calculate how much the player should move this frame and apply it.
+            /// </summary>
+            private void MovePlayer()
+            {
+                // get camera directions
+                Vector3 camRight = m_mainCamera.transform.right;
+                Vector3 camForward = m_mainCamera.transform.forward;
+
+                // 'flatten'
+                camRight.y = 0;
+                camForward.y = 0;
+
+                // re-normalise now value has been edited
+                camRight = camRight.normalized;
+                camForward = camForward.normalized;
+
+                // apply direction and speed
+                Vector3 horizontalRelativeInput = m_moveDirection.x * camRight * m_moveSpeed;
+                Vector3 verticalRelativeInput = m_moveDirection.y * camForward * m_moveSpeed;
+
+                // only do the boxcast once per frame
+                bool thisFrameIsGrounded = isGrounded;
+
+                // if player just landed, reset their vertical velocity to 0
+                if (!m_groundedLastFrame && thisFrameIsGrounded)
+                    m_verticalVelocity = Vector3.zero;
+
+                // update for next check
+                m_groundedLastFrame = thisFrameIsGrounded;
+
+                // if the player should jump, add the jump velocity
+                if (m_willJump)
+                {
+                    m_verticalVelocity += transform.up * m_jumpSpeed;
+                    m_willJump = false;
+                }
+
+                // apply gravity if player is NOT grounded
+                if (!thisFrameIsGrounded)
+                    m_verticalVelocity += m_gravity;
+
+                // calculate movement vector
+                Vector3 movement = horizontalRelativeInput + verticalRelativeInput + m_verticalVelocity;
+                
+                // move player
+                transform.Translate(movement * Time.deltaTime, Space.World);
+            }
+
+            [Header("Jumping")]
+            [SerializeField] private float m_jumpSpeed;
+            private bool m_groundedLastFrame;
+            private Vector3 m_verticalVelocity = Vector3.zero;
+            private bool m_willJump = false;
+
+            /// <summary>
+            /// If the player is standing on floor or another object.
+            /// </summary>
+            public bool isGrounded
+            {
+                get
+                {
+                    float radius = m_collider.radius;
+                    //Vector3 playerBottom = new Vector3
+                    if (Physics.BoxCast(transform.position, new Vector3(radius, 0.2f, radius), -transform.up, Quaternion.identity, m_collider.height / 2f))
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+
+            /// <summary>
+            /// Make the player jump next time MovePlayer is called.
+            /// </summary>
+            public void Jump(InputAction.CallbackContext context)
+            {
+                if (context.started && m_groundedLastFrame)
+                {
+                    m_willJump = true;
+                }
+            }
+
             [Header("Camera")]
-            [SerializeField] private Camera m_camera;
-            [SerializeField] private float m_lookSensitivity;
-            [SerializeField] private Vector2 m_cameraVerticalLook;
+            [SerializeField] private Camera m_mainCamera;
+            [SerializeField] private CinemachineVirtualCamera m_virtualCam;
             private bool m_cursorIsLocked = false;
             public bool cursorIsLocked
             {
@@ -49,57 +158,17 @@ namespace Millivolt
                     }
                 }
             }
-            private Vector2 m_lookDirection;
-            
-            void Start()
+
+            /// <summary>
+            /// Change the move-speed of the camera.
+            /// </summary>
+            /// <param name="hSpeed">Horizontal camera speed.</param>
+            /// <param name="vSpeed">Vertical camera speed.</param>
+            public void SetCameraSensitivity(float hSpeed, float vSpeed)
             {
-                m_characterController = GetComponent<CharacterController>();
-                cursorIsLocked = true;
-            }
-
-            private void Update()
-            {
-                LookPlayer();
-            }
-
-            private void FixedUpdate()
-            {
-                MovePlayer();
-            }
-
-            // Input System methods
-            public void Move(InputAction.CallbackContext context)
-            {
-                m_moveDirection = context.ReadValue<Vector2>();
-            }
-
-            public void Look(InputAction.CallbackContext context)
-            {
-                m_lookDirection = context.ReadValue<Vector2>();
-            }
-
-            // private methods
-            private void MovePlayer()
-            {
-                m_characterController.Move(new Vector3(m_moveDirection.x, 0, m_moveDirection.y) * m_moveSpeed * Time.deltaTime);
-            }
-
-            private void LookPlayer()
-            {
-                // keep value between 0 and 360
-                if (m_lookDirection.x >= 360f)
-                    m_lookDirection.x -= 360;
-                else if (m_lookDirection.x < 0)
-                    m_lookDirection.x += 360;
-
-                // clamp vertical look
-                m_lookDirection.y = Mathf.Clamp(m_lookDirection.y, m_cameraVerticalLook.x, m_cameraVerticalLook.y);
-
-                // apply vertical rotation
-                m_camera.transform.eulerAngles = Vector3.right * m_lookDirection.y;
-
-                // apply rotation
-                transform.Rotate(Vector3.up * m_lookDirection.x);
+                var pov = m_virtualCam.GetCinemachineComponent<CinemachinePOV>();
+                pov.m_HorizontalAxis.m_MaxSpeed = hSpeed;
+                pov.m_VerticalAxis.m_MaxSpeed = vSpeed;
             }
         }
     }
--- Assets/Player/Scripts/PlayerInteraction.cs
+++ Assets/Player/Scripts/PlayerInteraction.cs
@@ -11,6 +11,35 @@ namespace Millivolt
 {
 	public class PlayerInteraction : MonoBehaviour
 	{
-		
-	}
+		[SerializeField] private Collider m_trigger;
+
+        private void Start()
+        {
+			m_trigger.isTrigger = true;
+			m_trigger.enabled = false;
+        }
+
+        public void Interact()
+		{
+			m_trigger.enabled = true;
+		}
+
+        private void InteractWithInteractableObject(GameObject obj)
+        {
+            Debug.Log(obj.name + " was interacted with.");
+            m_trigger.enabled = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Interactable"))
+                InteractWithInteractableObject(other.gameObject);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Interactable"))
+                InteractWithInteractableObject(collision.gameObject);
+        }
+    }
 }
