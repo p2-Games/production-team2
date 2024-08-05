ede2411ef5d5aabe71b1b9c605c8df8814e04089
halenf <s210172@students.aie.edu.au>
2024-08-05 14:31:22 +0930

    More level objects
    Launchpads completed.
    Edited player controller to have a separate velocity for objects that control the player's velocity;
    Added Killzone script for triggers that instantly kill the player.

--- Assets/LevelObjects/Scripts/LaunchPad.cs
+++ Assets/LevelObjects/Scripts/LaunchPad.cs
@@ -5,8 +5,11 @@
 ///
 ///</summary>
 
+using System.Collections.Generic;
+using System.Xml;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 namespace Millivolt
 {
@@ -26,28 +29,69 @@ namespace Millivolt
                 }
             }
 
-            [SerializeField] private Vector3 m_launchForce;
+            [SerializeField] private Vector3 m_initialVelocity;
+
+            [Header("Debug"), SerializeField] private bool m_drawLines;
+            [SerializeField] private int m_debugPointsToDraw;
+            [SerializeField] private float m_debugTimeBetweenPoints;
+
+            private PlayerController m_player;
             private Collider m_trigger;
 
             private void Start()
             {
                 m_trigger = GetComponent<Collider>();
+                m_player = FindObjectOfType<PlayerController>();
             }
 
             private void OnTriggerEnter(Collider other)
-            {
-                PlayerController player = other.GetComponent<PlayerController>();
-                if (player)
+            {;
+                if (other.gameObject == m_player.gameObject)
                 {
-                    player.AddVelocity(m_launchForce);
+                    Vector3 newPlayerPosition = transform.position;
+                    newPlayerPosition.y += m_player.height / 2;
+                    m_player.transform.position = newPlayerPosition;
+                    m_player.SetExternalVelocity(m_initialVelocity);
                 }
             }
 
 #if UNITY_EDITOR
             private void OnDrawGizmos()
             {
+                if (!m_player)
+                    m_player = FindObjectOfType<PlayerController>();
+
                 Handles.color = Color.blue;
-                Handles.DrawLine(transform.position, transform.position + m_launchForce.normalized, m_launchForce.magnitude);
+
+                Vector3 lastPosition = transform.position;
+
+                for (int p = 1; p < m_debugPointsToDraw; p++)
+                {
+                    float t = p * m_debugTimeBetweenPoints;
+                    Vector3 nextPoint = m_initialVelocity;
+                    nextPoint *= t;
+                    nextPoint.y = m_initialVelocity.y * t + 0.5f * m_player.gravity * t * t;
+
+                    Vector3 position = transform.position + nextPoint;
+
+                    if (Physics.Raycast(lastPosition, (position - lastPosition).normalized, out RaycastHit hit, Vector3.Distance(lastPosition, position), ~(1 << LayerMask.NameToLayer("Player")), QueryTriggerInteraction.Ignore))
+                    {
+                        if (m_drawLines)
+                            Handles.DrawLine(lastPosition, hit.point);
+                        Handles.color = Color.red;
+                        Handles.DrawWireCube(hit.point, new Vector3(0.5f, 0.5f, 0.5f));
+                        break;
+                    }
+                    else
+                    {
+                        if (m_drawLines)
+                            Handles.DrawLine(lastPosition, position);
+                        else
+                            Handles.DrawWireCube(position, new Vector3(0.5f, 0.5f, 0.5f));
+                    }
+
+                    lastPosition = position;
+                }
             }
 #endif
         }
