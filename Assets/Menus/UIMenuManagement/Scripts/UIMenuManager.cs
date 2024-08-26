90ec21ad948d4ca243dbda925a7c04f55ccab93f
TobyAIE <126209021+TobyAIE@users.noreply.github.com>
2024-08-26 12:27:24 +0930

    UIManager and UIMenu class work
    UIManager has been updated to have a dynamic list of current active UIMenu objects
    UIMenu objects have activation and deactivation functions that work together with tweens
    Added selfdestruct script for Brayden

---
+++ Assets/Menus/UIMenuManagement/Scripts/UIMenuManager.cs
@@ -0,0 +1,57 @@
+///<summary>
+/// Author: Emily
+///
+/// A Manager for handling all menus in a scene
+///
+///</summary>
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Millivolt
+{
+	namespace UI
+	{
+		public class UIMenuManager : MonoBehaviour
+		{
+			public static UIMenuManager Instance;
+			private void Awake()
+            {
+				if (!Instance)
+					Instance = this;
+				else if (Instance != this)
+					Destroy(gameObject);
+
+				DontDestroyOnLoad(gameObject);
+            }
+
+			public List<UIMenu> activeMenus;
+
+			/// <summary>
+			/// This will update the status of UIMenu objects in the activeMenu's list
+			/// </summary>
+			/// <param name="menuIndex"></param>
+			public void SetActiveMenu()
+			{
+				for (int i = 0; i < activeMenus.Count; i++)
+				{
+					if (i != 0)
+					{
+						activeMenus[i].GetComponent<GraphicRaycaster>().enabled = false;
+						if (activeMenus[i].hideOnInactive)
+							activeMenus[i].gameObject.SetActive(false);
+					}
+					else
+						activeMenus[0].GetComponent<GraphicRaycaster>().enabled = true; activeMenus[0].gameObject.SetActive(true); 
+                }
+			}
+
+            private void Start()
+            {
+				//Check if the scene starts with a menu open and add it to the active scenes list
+				FindObjectOfType<UIMenu>().ActivateMenu();
+            }
+        }
+	}
+}
