fb8c6bbea949f83b067fe3e79fe76ef4571aa2e8
halenf <s210172@students.aie.edu.au>
2024-11-25 10:34:16 +1030

    Removed some namespaces

--- Assets/LevelObjects/Scripts/GravityChanger.cs
+++ Assets/LevelObjects/Scripts/GravityChanger.cs
@@ -10,36 +10,30 @@ using UnityEngine;
 
 namespace Millivolt
 {
-    namespace LevelObjects
-    {
-        namespace Events
-        {
-            public class GravityChanger : MonoBehaviour
-            {
-                [Tooltip("The acceleration of gravity in units per second squared.")]
-                [SerializeField] private float m_magnitude;
-
-                [Tooltip("The direction of gravity when this event triggers.")]
-                [SerializeField] private Vector3 m_direction;
-
-                /// <summary>
-                /// Changes the gravity for the Player and all rigidbodies in the scene.
-                /// </summary>
-                [ContextMenu("Set Physics.gravity")]
-                public void ChangeGravity()
-                {
-                    GameManager.Instance.ChangeGravity(m_direction, m_magnitude);
-                    SFXController.Instance.PlayRandomSoundClip("GravitySwitch", GameManager.Player.Controller.transform.position);
-                }
-
+     public class GravityChanger : MonoBehaviour
+     {
+         [Tooltip("The acceleration of gravity in units per second squared.")]
+         [SerializeField] private float m_magnitude;
+     
+         [Tooltip("The direction of gravity when this event triggers.")]
+         [SerializeField] private Vector3 m_direction;
+     
+         /// <summary>
+         /// Changes the gravity for the Player and all rigidbodies in the scene.
+         /// </summary>
+         [ContextMenu("Set Physics.gravity")]
+         public void ChangeGravity()
+         {
+             GameManager.Instance.ChangeGravity(m_direction, m_magnitude);
+             SFXController.Instance.PlayRandomSoundClip("GravitySwitch", GameManager.Player.Controller.transform.position);
+         }
+     
 #if UNITY_EDITOR
-                private void OnDrawGizmos()
-                {
-                    Handles.color = Color.yellow;
-                    Handles.ArrowHandleCap(0, transform.position, Quaternion.Euler(m_direction) * Quaternion.FromToRotation(Vector3.forward, Vector3.up), 1.4f, EventType.Repaint);
-                }
+         private void OnDrawGizmos()
+         {
+             Handles.color = Color.yellow;
+             Handles.ArrowHandleCap(0, transform.position, Quaternion.Euler(m_direction) * Quaternion.FromToRotation(Vector3.forward, Vector3.up), 1.4f, EventType.Repaint);
+         }
 #endif
-            }
-        }
     }
 }
