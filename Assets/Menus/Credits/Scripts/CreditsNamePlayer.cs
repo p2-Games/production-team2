9e2f3bfba37c82bff590d201a46d2518a6b1dbf5
EmilyAIE <126209021+EmilyAIE@users.noreply.github.com>
2024-12-04 10:27:45 +1030

    Credits Screen polishing
    Credit names now fade in/out
    Added a full credits wall
    Added skip to credits

--- Assets/Menus/Credits/Scripts/CreditsNamePlayer.cs
+++ Assets/Menus/Credits/Scripts/CreditsNamePlayer.cs
@@ -9,7 +9,6 @@ using Pixelplacement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Millivolt.UI
@@ -24,6 +23,10 @@ namespace Millivolt.UI
 
 		[SerializeField] private GameObject m_creditsPrefab;
 
+        [Header("Credits Details")]
+        [SerializeField] private float m_creditNameFadeInTime;
+        [SerializeField] private float m_creditNameFadeOutTime;
+
         [Header("Menu Button")]
         [SerializeField] private Button m_menuButton;
 
@@ -31,9 +34,16 @@ namespace Millivolt.UI
         [SerializeField] private GameObject m_spotLight;
         [SerializeField] private float m_spotlightActivationTime;
 
+        [Header("CreditsWall Details")]
+        [SerializeField] private CanvasGroup m_creditsWallCG;
+        [SerializeField] private float m_creditsWallFadeTime;
+
+        private bool m_playingCredits;
+
         private void Start()
         {
             Invoke(nameof(ActivateSpotLight), m_spotlightActivationTime);
+            m_playingCredits = true;
             foreach (CreditsName person in m_peopleCredits)
             {
                 StartCoroutine(PlayCreditsText(person));
@@ -47,6 +57,7 @@ namespace Millivolt.UI
             GameObject credit = Instantiate(m_creditsPrefab, gameObject.transform);
             credit.transform.position = person.spawnPosition.position;
             CreditsPrefabReferences creditsRef = credit.GetComponent<CreditsPrefabReferences>();
+            Tween.CanvasGroupAlpha(creditsRef.GetComponent<CanvasGroup>(), 1, m_creditNameFadeInTime, 0);
             creditsRef.nameText.text = person.nameText;
             creditsRef.jobText.text = person.jobTitleText;
             creditsRef.bgImage.sprite = person.bgImage;
@@ -62,10 +73,17 @@ namespace Millivolt.UI
             {
                 m_menuButton.gameObject.SetActive(true);
                 Tween.CanvasGroupAlpha(m_menuButton.GetComponent<CanvasGroup>(), 1, 1, 0, null, Tween.LoopType.None, null, () => { SelectButton(); });
+                Tween.CanvasGroupAlpha(m_creditsWallCG, 1, m_creditsWallFadeTime, 1);
+                m_playingCredits = false;
             }
 
-            Destroy(credit);
+            Tween.CanvasGroupAlpha(creditsRef.GetComponent<CanvasGroup>(), 0, m_creditNameFadeOutTime, 0, null, Tween.LoopType.None, null, () => { DestroyCredit(credit); });
             yield return null;
+        }
+
+        private void DestroyCredit(GameObject credit)
+        {
+            Destroy(credit);
         }
 
         private void SelectButton()
@@ -81,6 +99,15 @@ namespace Millivolt.UI
         private void ActivateSpotLight()
         {
             m_spotLight.SetActive(true);
+        }
+
+        public void SkipCredits()
+        {
+            StopAllCoroutines();
+            m_menuButton.gameObject.SetActive(true);
+            Tween.CanvasGroupAlpha(m_menuButton.GetComponent<CanvasGroup>(), 1, 1, 0, null, Tween.LoopType.None, null, () => { SelectButton(); });
+            Tween.CanvasGroupAlpha(m_creditsWallCG, 1, m_creditsWallFadeTime, 0);
+            m_playingCredits = false;
         }
     }
 }
